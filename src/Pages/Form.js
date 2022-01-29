66dff35ebfcb6749c4120baf96fb9ad719f49404
tempweb <57166335+shaansrivastava2001@users.noreply.github.com>
2022-01-29 10:16:22 +0530

    Add files via upload

---
+++ src/Pages/Form.js
@@ -0,0 +1,16 @@
+import React from "react";
+import FormBody from "../Components/formBody";
+import Header from "../Components/header";
+import Footer from "../Components/footer";
+
+function Form() {
+  return (
+    <div>
+      <Header />
+      <FormBody />
+      <Footer />
+    </div>
+  );
+}
+
+export default Form;
